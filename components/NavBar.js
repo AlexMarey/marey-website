06d7b760ffb7243685504f86abbd889338f909dc
Alex Marey <armv26@mst.edu>
2021-01-25 23:02:46 -0500

    Starting to style NavBar

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,16 +1,36 @@
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import NavButton from './NavButton.js';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+
+const useStyles = makeStyles((theme) => ({
+    toolbar: {
+        justifyContent: 'space-between',
+        overflowX: 'auto',
+    },
+    toolbarTitle: {
+        flex: 1,
+        alignContent: 'left',
+    },
+    toolbarButtons: {
+        alignContent:'right',
+        justifyContent: 'space-between',
+    }
+}));
 
 export default function Navigation() {
-    
-    return( 
-        <Toolbar>
-            <Typography variant="h4">Marey</Typography>
+    const classes = useStyles();
 
-            <NavButton text={"About Me"}/>
-            <NavButton text={"Posts"}/>
-            <NavButton text={"Contact Me"}/>
+    return( 
+        <Toolbar className={classes.toolbar}>
+            <Typography variant="h4" className={classes.toolbarTitle}>Marey</Typography>
+            <Box className={classes.toolbarButtons}>
+                <NavButton text={"About Me"}/>
+                <NavButton text={"Posts"}/>
+                <NavButton text={"Contact Me"}/>
+            </Box>
+
         </Toolbar>
     );
 }
