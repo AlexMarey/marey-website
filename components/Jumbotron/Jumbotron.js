d1e6cc65b9aebf6614ec095d9e6fe280c895838f
Alex Marey <alx.9.mar@gmail.com>
2021-02-01 22:13:18 -0500

    Updated Jumbotron spacing above picture

--- components/Jumbotron/Jumbotron.js
+++ components/Jumbotron/Jumbotron.js
@@ -4,15 +4,15 @@ import Typography from "@material-ui/core/Typography";
 
 export default function Jumbotron() {
   return (
-    <>
+    <Box m={2}>
       <Box display="flex"> 
-        <Box className={styles.imgWrapper} m="auto" p={2}>
+        <Box className={styles.imgWrapper} m="auto" pt={2}>
           <img className={styles.imgCropper} src="/alex-square-edit.jpg"/>
         </Box>
       </Box>
       <Box display="flex" alignItems="center" justifyContent="center">
         <Typography variant="h4" gutterBottom>Welcome! Enjoy your stay.</Typography>
       </Box>
-    </>
+    </Box>
   );
 }
