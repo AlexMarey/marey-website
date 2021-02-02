da306ef7c024eecc647e691dff116e2e63ec31e7
Alex Marey <alx.9.mar@gmail.com>
2021-02-01 22:11:08 -0500

    Finished spacing and material ui upgrade for About Me Section

--- components/AboutMe/AboutMe.js
+++ components/AboutMe/AboutMe.js
@@ -1,17 +1,25 @@
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 
+import styles from './AboutMe.module.css'
+
+const factsAboutMe = [
+  'Raised and born in St.Louis, finding a new home on the East coast',
+  'Software developer by day with a sweet tooth for excellent user experience',
+  'Doesn\'t matter if I\'m playing video games or board games as long as I\'m in good company',
+  'In my spare time I like to be creative by taking photos or drawing',
+  'The beach is better than mountains',
+  'Running, volleyball, yoga, skateboarding, or bouldering. You name it, I want to try it.',
+]
+
 export default function AboutMe() {
   return (
-    <Box>
+    <Box m={2} pt={2} pb={2}>
       <Typography variant='h6' gutterBottom>This is my life</Typography>
-      <ul>
-          <li>Raised and born in St.Louis, finding a new home on the East coast</li>
-          <li>Software developer by day with a sweet tooth for excellent user experience</li>
-          <li>Doesn't matter if I'm playing video games or board games as long as I'm in good company</li>
-          <li>In my spare time I like to be creative by taking photos or drawing</li>
-          <li>The beach is better than mountains</li>
-          <li>Running, volleyball, yoga, skateboarding, or bouldering. You name it, I want to try it.</li>
+      <ul className={styles.asterisk}>
+          {factsAboutMe.map((fact) => {
+            return <li><Typography variant="body">{fact}</Typography></li>
+          })}
       </ul>
     </Box>
   );
