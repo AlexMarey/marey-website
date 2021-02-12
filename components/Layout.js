725413d0d984b8d5183fb6e9994a85c8f6580e34
Alex Marey <alx.9.mar@gmail.com>
2021-02-12 12:11:13 -0500

    Removed page container for individual component containers

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,12 @@
 import Navbar from "./NavBar";
-import Container from "@material-ui/core/Container";
-import CssBaseline from '@material-ui/core/CssBaseline';
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 export default function Layout({ children }) {
   return (
     <React.Fragment>
       <CssBaseline />
       <Navbar />
-      <Container maxWidth="lg">{children}</Container>
+      {children}
     </React.Fragment>
   );
 }
