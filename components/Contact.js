5b46018faca60a72f6c8436a5e1ac32bb97ff4aa
Alex Marey <alx.9.mar@gmail.com>
2021-02-12 15:50:07 -0500

    Completed sticky footer

--- components/Contact.js
+++ components/Contact.js
@@ -16,28 +16,26 @@ export default function Contact() {
   const classes = useStyles();
 
   return (
-    <Box
-      display="flex"
-      flexWrap="wrap"
-      alignItems="center"
-      justifyContent="center"
-      color="#e3e3e3"
-      bgcolor="#233142"
-    >
-      <Typography variant="h6" gutterBottom>
-        Want to chat? Reach out to me on Twitter
-      </Typography>
-      <Box className="break" />
-      {/* <Link href="mailto:alx.9.mar@gmail.com">
-        <a>
-          <EmailIcon className={classes.icon} />
-        </a>
-      </Link> */}
-      <Link href={"https://twitter.com/AlexTakesOnCHS"}>
-        <a>
-          <TwitterIcon className={classes.icon} />
-        </a>
-      </Link>
-    </Box>
+    <footer className="footer">
+      <Box
+        display="flex"
+        flexWrap="wrap"
+        alignItems="center"
+        justifyContent="center"
+        height="7rem"
+        color="#e3e3e3"
+        bgcolor="#233142"
+      >
+        <Typography variant="h6" gutterBottom>
+          Want to chat? Reach out to me on Twitter
+        </Typography>
+        <Box className="break" />
+        <Link href={"https://twitter.com/AlexTakesOnCHS"}>
+          <a>
+            <TwitterIcon className={classes.icon} />
+          </a>
+        </Link>
+      </Box>
+    </footer>
   );
 }
--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,15 @@
 import Navbar from "./NavBar";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import Box from "@material-ui/core/Box";
 
 export default function Layout({ children }) {
   return (
     <React.Fragment>
       <CssBaseline />
-      <Navbar />
-      {children}
+      <Box className="pageContainer">
+        <Navbar />
+        {children}
+      </Box>
     </React.Fragment>
   );
 }
