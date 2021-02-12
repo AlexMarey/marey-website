454c4dec8fafcc0802aa281cef4c053bdce570bb
Alex Marey <alx.9.mar@gmail.com>
2021-02-12 16:17:55 -0500

    Updated theme to change the background color

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,17 @@
 import Navbar from "./NavBar";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Box from "@material-ui/core/Box";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import alexTheme from "../src/theme";
 
 export default function Layout({ children }) {
   return (
-    <React.Fragment>
+    <MuiThemeProvider theme={alexTheme}>
       <CssBaseline />
       <Box className="pageContainer">
         <Navbar />
         {children}
       </Box>
-    </React.Fragment>
+    </MuiThemeProvider>
   );
 }
